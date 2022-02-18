08c01b3b79eb10e46b4a5cc0b010990fbfee2030
sujal631 <99301344+sujal631@users.noreply.github.com>
2022-02-18 15:23:58 -0600

    Revert "Add to cart action"
    This reverts commit 83460bae2ce3fb698fa6bc6538d13f6c66248952.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,38 +1,28 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
-import { BrowserRouter, Link, Route } from 'react-router-dom';
-import CartScreen from './screens/CartScreen';
+import { BrowserRouter, Route } from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 
 function App() {
-  const cart = useSelector((state) => state.cart);
-  const { cartItems } = cart;
   return (
     <BrowserRouter>
       <div className="grid-container">
         <header className="row">
           <div>
-            <Link className="brand" to="/">
+            <a className="brand" href="/">
               myApparel.com
-            </Link>
+            </a>
           </div>
           <div>
-            <Link to="/cart">
-              Cart
-              {cartItems.length > 0 && (
-                <span className="badge">{cartItems.length}</span>
-              )}
-            </Link>
-            <Link to="/signin">Sign In</Link>
+            <a href="/cart">Cart</a>
+            <a href="/signin">Sign In</a>
           </div>
         </header>
         <main>
-          <Route path="/cart/:id?" component={CartScreen}></Route>
           <Route path="/product/:id" component={ProductScreen}></Route>
           <Route path="/" component={HomeScreen} exact></Route>
         </main>
-        <footer className="row center">All rights reserved</footer>
+        <footer className="row center">All right reserved</footer>
       </div>
     </BrowserRouter>
   );
--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,18 +1,17 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import Rating from './Rating';
 
 export default function Product(props) {
   const { product } = props;
   return (
     <div key={product._id} className="card">
-      <Link to={`/product/${product._id}`}>
+      <a href={`/product/${product._id}`}>
         <img className="medium" src={product.image} alt={product.name} />
-      </Link>
+      </a>
       <div className="card-body">
-        <Link to={`/product/${product._id}`}>
+        <a href={`/product/${product._id}`}>
           <h2>{product.name}</h2>
-        </Link>
+        </a>
         <Rating
           rating={product.rating}
           numReviews={product.numReviews}
