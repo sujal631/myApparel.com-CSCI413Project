d7fb4c7ad2a8accab671b2346d21faadfa0ed753
Praneetha Gobburi <praneethag00@gmail.com>
2022-03-01 00:26:43 -0600

    place order screen and api created

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import productRouter from './routers/productRouter.js';
 import userRouter from './routers/userRouter.js';
+import orderRouter from './routers/orderRouter.js';
 
 dotenv.config();
 
@@ -35,6 +36,7 @@ app.get('/api/products', (req, res) => {
 
 app.use('/api/products', productRouter);
 app.use('/api/users', userRouter);
+app.use('/api/orders', orderRouter);
 
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
