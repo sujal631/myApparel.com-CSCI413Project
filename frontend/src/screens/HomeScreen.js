67d82c3fba58b2cea5044f296208eb5d88666b02
sujal631 <99301344+sujal631@users.noreply.github.com>
2022-04-04 11:18:48 -0500

    update

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -48,7 +48,7 @@ export default function HomeScreen() {
           </Carousel>
         </>
       )}
-      <h2>Featured Products</h2>
+      <h2>Newest Arrivals</h2>
       {loading ? (
         <LoadingBox></LoadingBox>
       ) : error ? (
